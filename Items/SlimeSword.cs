cfea1b9c2867e58b22bd6e98745763f11dde8af6
Caleb-sbani <67887509+Caleb-sbani@users.noreply.github.com>
2021-11-15 21:40:30 -0500

    adds basic sword
    Creates SlimeSword, which is a basic sword created from 50 slime at an anvil

---
+++ Items/SlimeSword.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace Slime.Items
+{
+    class SlimeSword : ModItem
+    {
+        public override void SetDefaults()
+        {
+            item.damage = 20;
+            item.useTime = 20;
+            item.useAnimation = 20;
+            item.useStyle = ItemUseStyleID.SwingThrow;
+        }
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.Gel, 50);
+            recipe.AddIngredient(ItemID.Ruby, 4);
+            recipe.AddIngredient(ItemID.Wood, 10);
+            recipe.AddTile(TileID.Solidifier);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
