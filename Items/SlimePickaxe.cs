a6ccde54b46b34131a33cbae2b2451b9be5277dc
Caleb-sbani <67887509+Caleb-sbani@users.noreply.github.com>
2021-11-16 17:08:00 -0500

    added a slime pickaxe!
    mines quicker as you kill more slimes, getting insta mine at 9000 kills

---
+++ Items/SlimePickaxe.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Slime;
+
+namespace Slime.Items
+{
+    class SlimePickaxe : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Slime Pickaxe");
+            Tooltip.SetDefault("Ewww.... Sticky");
+        }
+        public override void SetDefaults()
+        {
+            item.damage = 10;
+            item.useTime = 20;
+            item.useAnimation = 13;
+            item.pick = 65;
+            item.autoReuse = true;
+            item.useStyle = ItemUseStyleID.SwingThrow;
+        }
+        public override void ModifyWeaponDamage(Terraria.Player player, ref float add, ref float mult)
+        {
+            item.useTime = 20 - player.GetModPlayer<Player>().SlimeKills/450;
+        }
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.Gel, 15);
+            recipe.AddIngredient(ItemID.GoldBar, 10);
+            recipe.AddIngredient(ItemID.Ruby, 1);
+            recipe.AddTile(TileID.Solidifier);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
